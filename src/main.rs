9935f6705302bf3538cc3b31a329462637f657fd
Robin Smekens <robin.smekens.930@gmail.com>
2022-10-15 12:02:04 +0200

    added collision handling for walls

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use bevy::{
     prelude::*,
-    //sprite::collide_aabb::{collide, Collision},
+    sprite::collide_aabb::{collide, Collision},
     time::FixedTimestep
 };
 
@@ -11,10 +11,12 @@ const TIME_STEP: f32 = 1.0 / 60.0;
 const PADDLE_SIZE: Vec3 = Vec3::new(120.0, 20.0, 0.0);
 const PADDLE_SPEED : f32 = 500.0;
 const PADDLE_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
+const PADDLE_BOTTOM_OFFSET : f32 = 25.0;
 
 // PROJECTILE DATA
 const PROJECTILE_SIZE: Vec3 = Vec3::new(20.0, 20.0, 20.0);
 const PROJECTILE_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
+const PROJECITLE_SPEED: f32 = 1000.0;
 const INITIAL_BALL_DIRECTION: Vec2 = Vec2::new(0.0, 1.0);
 
 // x coordinates
@@ -23,6 +25,8 @@ const RIGHT_WALL: f32 = 600.;
 // y coordinates
 const BOTTOM_WALL: f32 = -300.;
 const TOP_WALL: f32 = 300.;
+const WALL_THICKNESS: f32 = 10.0;
+const WALL_COLOR: Color = Color::rgb(0.8, 0.8, 0.8);
 
 // UI DATA
 const SCOREBOARD_FONT_SIZE: f32 = 40.0;
@@ -36,8 +40,6 @@ fn main()  {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(SpaceInvader)
-        .insert_resource(ClearColor(BACKGROUND_COLOR))
-        .add_system(bevy::window::close_on_esc)
         .run();
 }
 
@@ -47,14 +49,19 @@ impl Plugin for SpaceInvader{
     fn build(&self, app: &mut App) {
         app
         .add_startup_system(setup)
+        .insert_resource(ClearColor(BACKGROUND_COLOR))
+        .add_event::<CollisionEvent>()
         .add_system_set(
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
-                .with_system(move_player)
-                .with_system(system_spawn_projectile)
-                .with_system(system_apply_velocity)
+                .with_system(move_player.before(check_for_collisions))
+                .with_system(system_spawn_projectile.before(check_for_collisions))
+                .with_system(system_apply_velocity.before(check_for_collisions))
+                .with_system(check_for_collisions.before(system_lifetime))
                 .with_system(system_lifetime)
-        );
+                .with_system(play_collision_sound.after(system_lifetime))
+        )
+        .add_system(bevy::window::close_on_esc);
     }
 }
 
@@ -70,16 +77,99 @@ struct Projectile;
 #[derive(Component, Deref, DerefMut)]
 struct LifeTime(f32);
 
+#[derive(Component)]
+struct Collider;
+
+#[derive(Default)]
+struct CollisionEvent;
+
+struct CollisionSound(Handle<AudioSource>);
+
+#[derive(Bundle)]
+struct WallBundle {
+    #[bundle]
+    sprite_bundle: SpriteBundle,
+    collider: Collider,
+}
+
+enum WallLocation {
+    Left,
+    Right,
+    Bottom,
+    Top
+}
+
+impl WallLocation {
+    fn position(&self) -> Vec2 {
+        match self {
+            WallLocation::Left => Vec2::new(LEFT_WALL, 0.0),
+            WallLocation::Right => Vec2::new(RIGHT_WALL, 0.0),
+            WallLocation::Bottom => Vec2::new(0.0, BOTTOM_WALL),
+            WallLocation::Top => Vec2::new(0.0, TOP_WALL),
+        }
+    }
+
+    fn size(&self) -> Vec2 {
+        let arena_height = TOP_WALL - BOTTOM_WALL;
+        let arena_width = RIGHT_WALL - LEFT_WALL;
+
+        assert!(arena_height > 0.0);
+        assert!(arena_width > 0.0);
+
+        match self {
+            WallLocation::Left | WallLocation::Right => {
+                Vec2::new(WALL_THICKNESS, arena_height + WALL_THICKNESS)
+            }
+            WallLocation::Bottom | WallLocation::Top => {
+                Vec2::new(arena_width + WALL_THICKNESS, WALL_THICKNESS)
+            }
+        }
+    }
+}
+
+impl WallBundle {
+    // This "builder method" allows us to reuse logic across our wall entities,
+    // making our code easier to read and less prone to bugs when we change the logic
+    fn new(location: WallLocation) -> WallBundle {
+        WallBundle {
+            sprite_bundle: SpriteBundle {
+                transform: Transform {
+                    // We need to convert our Vec2 into a Vec3, by giving it a z-coordinate
+                    // This is used to determine the order of our sprites
+                    translation: location.position().extend(0.0),
+                    // The z-scale of 2D objects must always be 1.0,
+                    // or their ordering will be affected in surprising ways.
+                    // See https://github.com/bevyengine/bevy/issues/4149
+                    scale: location.size().extend(1.0),
+                    ..default()
+                },
+                sprite: Sprite {
+                    color: WALL_COLOR,
+                    ..default()
+                },
+                ..default()
+            },
+            collider: Collider,
+        }
+    }
+}
+
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // Camera
     commands.spawn_bundle(Camera2dBundle::default());
 
+    // Sound
+    let ball_collision_sound = asset_server.load("sounds/breakout_collision.ogg");
+    commands.insert_resource(CollisionSound(ball_collision_sound));
+
+
+    // Spawning player
     commands
         .spawn()
         .insert(Player)
         .insert_bundle(SpriteBundle {
             transform: Transform {
-                translation: Vec3::new(0.0, 0.0, 0.0),
+                translation: Vec3::new(0.0, BOTTOM_WALL + PADDLE_BOTTOM_OFFSET, 0.0),
                 scale: PADDLE_SIZE,
                 ..default()
             },
@@ -89,6 +179,12 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             },
             ..default()
         });
+
+    // Walls
+    commands.spawn_bundle(WallBundle::new(WallLocation::Left));
+    commands.spawn_bundle(WallBundle::new(WallLocation::Right));
+    commands.spawn_bundle(WallBundle::new(WallLocation::Bottom));
+    commands.spawn_bundle(WallBundle::new(WallLocation::Top));
 
      // Scoreboard
     commands.spawn_bundle(
@@ -149,7 +245,7 @@ fn system_spawn_projectile(mut commands: Commands, keyboard_input: Res<Input<Key
         commands
         .spawn()
         .insert(Projectile)
-        .insert(Velocity(INITIAL_BALL_DIRECTION.normalize()))
+        .insert(Velocity(INITIAL_BALL_DIRECTION.normalize() * PROJECITLE_SPEED))
         .insert(LifeTime(3.0))
         .insert_bundle(SpriteBundle {
             transform: Transform {
@@ -168,8 +264,8 @@ fn system_spawn_projectile(mut commands: Commands, keyboard_input: Res<Input<Key
 
 fn system_apply_velocity(mut query: Query<(&mut Transform, &Velocity)>) {
     for (mut transform, velocity) in &mut query {
-        transform.translation.x += velocity.x;
-        transform.translation.y += velocity.y;
+        transform.translation.x += velocity.x * TIME_STEP;
+        transform.translation.y += velocity.y * TIME_STEP;
     }
 }
 
@@ -179,6 +275,64 @@ fn system_lifetime(mut commands: Commands, mut query: Query<(Entity, &mut LifeTi
         if lifetime.0 <= 0.0 {
             commands.entity(entity).despawn();
         }
+    }
+}
+
+fn check_for_collisions(
+    mut _commands: Commands,
+    mut projectile_query: Query<(&Transform, &mut LifeTime), With<Projectile>>,
+    collider_query: Query<(Entity, &Transform), With<Collider>>,
+    mut collision_events: EventWriter<CollisionEvent>
+) {
+    if projectile_query.is_empty() {
+        return;
+    }
+
+    let (projectile_transform, mut lifetime) = projectile_query.single_mut();
+    let projectile_size = projectile_transform.scale.truncate();
+
+    for (_collider_entity, transform) in &collider_query {
+        let collision = collide(
+            projectile_transform.translation,
+            projectile_size,
+            transform.translation,
+            transform.scale.truncate(),
+        );
+
+        if let Some(collision) = collision {
+            // Sends a collision event so that other systems ca react to the collision
+            collision_events.send_default();
+            
+            // reflect the ball when it collides
+            let mut destroy_projectile = false;
+
+            // only reflect if the ball's velocity is going in the opposite direction of the collision
+            match collision {
+                Collision::Left => destroy_projectile = true,
+                Collision::Right => destroy_projectile = true,
+                Collision::Top => destroy_projectile = true,
+                Collision::Bottom => destroy_projectile = true,
+                Collision::Inside => { /* do nothing */}
+            }
+
+            if destroy_projectile {
+                lifetime.0 = 0.0;
+            }
+        }
+    }
+
+}
+
+fn play_collision_sound(
+    collision_events: EventReader<CollisionEvent>,
+    audio: Res<Audio>,
+    sound: Res<CollisionSound>,
+) {
+    // Play a sound once per frame if a collision occurred
+    if !collision_events.is_empty() {
+        // This prevents events staying active on the next frame
+        collision_events.clear();
+        audio.play(sound.0.clone());
     }
 }
 
